5c62f2b9979a974599a19795991e74336a5813cb
kevxcyj <kevcyjunior@gmail.com>
2025-07-17 09:42:20 -0500

    Updated index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import AppController from '../controllers/AppController.js';
+import UsersController from '../controllers/UsersController.js';
 
 const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+router.get('/users', UsersController.getUsers);
+router.post('/users', UsersController.createUser);
+
 
 export default router;
