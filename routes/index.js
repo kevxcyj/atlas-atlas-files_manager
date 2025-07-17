9c440954f413e67079df2cb6e58ab63b8ef47366
kevxcyj <kevcyjunior@gmail.com>
2025-07-17 09:55:01 -0500

    Updated index, created authcontroller

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import AppController from '../controllers/AppController.js';
 import UsersController from '../controllers/UsersController.js';
+import UsersController from '../controllers/UsersController.js';
 
 const router = express.Router();
 
